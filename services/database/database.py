a8489e7c1be83351d45ce989874a5e1c9b8c7b04
AgzamxodjayevBilolxoja <agzamxodjayevbilolxoja@gmail.com>
2025-04-29 19:18:17 +0500

    database.py faylini to'g'irladim

--- services/database/database.py
+++ services/database/database.py
@@ -5,8 +5,11 @@ class Database:
         self.__conn = sqlite3.connect('database.db')
         self.__cur = self.__conn.cursor()
     
-    def execute(self, sql, parametrs, commit, fetchall, fetchone):
-        data = self.__cur.execute(sql, parametrs)
+    def execute(self, sql: str, parametrs: tuple = (1,), commit: bool=False, fetchall: bool=False, fetchone: bool=False):
+        if parametrs:
+            data = self.__cur.execute(sql, parametrs)
+        else:
+            data = self.__cur.execute(sql)
         
         if commit:
             self.__conn.commit()
